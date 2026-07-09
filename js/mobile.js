9edc3c5a7f2847a709922918f8a3daf08e5293ce
yunmy <yd9817@gmail.com>
2026-07-09 17:13:14 +0900

    미두 스칼프 업데이트

--- js/mobile.js
+++ js/mobile.js
@@ -1,10 +1,21 @@
 $(function () {
-			function mobile1() {
-				$(".mobile > .mobile_box > ul").stop().animate({ marginTop: -510 }, function () {
-					$(".mobile > .mobile_box > ul > li:first").appendTo(".mobile > .mobile_box > ul")
-					$(".mobile > .mobile_box > ul").css({ marginTop: 0 })
-				})
-			}
-			setInterval(mobile1, 3000);
+	if (document.querySelector('.mobile-swiper')) {
+		new Swiper('.mobile-swiper', {
+			direction: 'vertical',
+			loop: true,
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			speed: 600,
+		});
+	}
 
+	function mobile1() {
+		$(".mobile > .mobile_box > ul").stop().animate({ marginTop: -510 }, function () {
+			$(".mobile > .mobile_box > ul > li:first").appendTo(".mobile > .mobile_box > ul")
+			$(".mobile > .mobile_box > ul").css({ marginTop: 0 })
 		})
+	}
+	setInterval(mobile1, 3000);
+})
